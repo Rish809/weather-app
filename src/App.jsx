e3ed620fe68b38db048cf0de1d37033d15d949b6
Rish809 <r33772871@gmail.com>
2025-05-23 13:25:16 +0530

    feature 5 added

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,17 @@ function App() {
       setvalue(value + 1);
     }
    } >Add</button>
+   <button onClick={
+    ()=>{
+      setvalue(value + 1);
+    }
+   } >Add2</button>
+    <button onClick={
+    ()=>{
+      setvalue(value + 1);
+    }
+   } >Add3</button>
    </>
   )
 }
-
 export default App
